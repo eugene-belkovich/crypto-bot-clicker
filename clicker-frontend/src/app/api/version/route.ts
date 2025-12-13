d376c6485cb21c10b01d1f703575b912dd45088f
Evgeniy Belkovich <eugene.belkovich@gmail.com>
2025-12-12 22:59:39 -0300

    fix(front): redundant api removed

--- clicker-frontend/src/app/api/version/route.ts
+++
@@ -1,8 +0,0 @@
-import {NextResponse} from 'next/server';
-
-export async function GET() {
-  return NextResponse.json({
-    name: 'clicker-frontend',
-    version: process.env.npm_package_version || '0.0.1'
-  });
-}
